fb3ae085847643aeea883254a8e1fddcebb6b226
LB-Squishy <laurentleboulbar@gmail.com>
2025-11-07 18:41:27 +0100

    feat: Ajout de la section produits sur la page d'accueil avec mise en forme et affichage des détails des produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
+    public function __construct(
+        private ProductRepository $productRepository,
+    ) {}
+
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
+        $products = $this->productRepository->findBy(['isActive' => true], ['createdAt' => 'DESC'], 9);
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
         ]);
     }
 }
